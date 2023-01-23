a8f6eb53da776811c2ea4f2649e8b031f44d2225
Mario Dohr <mario.dohr@gmail.com>
2023-01-23 11:24:00 +0100

    new comment

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from model import db
 
-
+# new comment
 def create_app():
     app = Flask(__name__, template_folder='../templates', instance_relative_config=True)
     app.logger.info('create_app')
